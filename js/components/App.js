d1b049a011115cfb36986a61456be3ab125a19df
taroshuntaro <shoji.dev.shuntaro@gmail.com>
2019-12-22 21:55:42 +0900

    これらにインストールし直し ⇒ @babel/core @babel/cli @babel/preset-env @babel/preset-react ※jsx監視コマンド実行成功

---
+++ js/components/App.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var App = function App() {
+  return React.createElement("h1", null, "Hello.");
+};
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
