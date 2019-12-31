8b97809580ff9989315fba85541f607e90b06cec
shuntaro shoji <160341ss@gmail.com>
2020-01-01 00:07:33 +0900

    react router、ひとまず動く状態

--- manage/js/components/App.js
+++ manage/js/components/App.js
@@ -5,42 +5,43 @@ define(function (require, exports, module) {
 
   var ReactDOM = require("react-dom");
 
-  var BrowserRouter = require("react-router-dom").BrowserRouter;
+  var HashRouter = require("react-router-dom").HashRouter;
 
   var Route = require("react-router-dom").Route;
 
   var Link = require("react-router-dom").Link;
 
+  var Home = function Home() {
+    return React.createElement("div", null, React.createElement("h2", null, "Home"), "Home\u30DA\u30FC\u30B8\u3067\u3059");
+  };
+
+  var About = function About() {
+    return React.createElement("div", null, React.createElement("h2", null, "About"), "About\u30DA\u30FC\u30B8\u3067\u3059");
+  };
+
   var App = function App() {
-    var Home = function Home() {
-      return React.createElement("div", null, React.createElement("h2", null, "Home"), "Home\u30DA\u30FC\u30B8\u3067\u3059");
-    };
-
-    var About = function About() {
-      return React.createElement("div", null, React.createElement("h2", null, "About"), "About\u30DA\u30FC\u30B8\u3067\u3059");
-    };
-
     return React.createElement("div", {
       className: "app container"
     }, React.createElement("h1", {
       className: "display-1"
     }, "Hello World!", React.createElement("span", {
       className: "badge badge-secondary"
-    }, "New")), React.createElement(BrowserRouter, null, React.createElement("div", {
+    }, "New")), React.createElement(HashRouter, null, React.createElement("div", {
       className: "list container"
     }, React.createElement("ul", {
       className: "list-group"
     }, React.createElement("li", {
       className: "list-group-item"
     }, React.createElement(Link, {
-      to: "/"
+      to: "/home"
     }, "Home")), React.createElement("li", {
       className: "list-group-item"
     }, React.createElement(Link, {
       to: "/about"
-    }, "About"))), React.createElement(Route, {
-      exact: true,
-      path: "/",
+    }, "About")))), React.createElement("div", {
+      className: "container"
+    }, React.createElement(Route, {
+      path: "/home",
       component: Home
     }), React.createElement(Route, {
       path: "/about",
--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
 define(function(require, exports, module) {
   const React = require("react");
   const ReactDOM = require("react-dom");
-  const BrowserRouter = require("react-router-dom").BrowserRouter;
+  const HashRouter = require("react-router-dom").HashRouter;
   const Route = require("react-router-dom").Route;
   const Link = require("react-router-dom").Link;
 
-  const App = () => {
-    const Home = () => (
-      <div>
-        <h2>Home</h2>
-        Homeページです
-      </div>
-    );
-    const About = () => (
-      <div>
-        <h2>About</h2>
-        Aboutページです
-      </div>
-    );
+  const Home = () => (
+    <div>
+      <h2>Home</h2>
+      Homeページです
+    </div>
+  );
+  const About = () => (
+    <div>
+      <h2>About</h2>
+      Aboutページです
+    </div>
+  );
 
+  const App = () => {
     return (
       <div className="app container">
         <h1 className="display-1">
           Hello World!
           <span className="badge badge-secondary">New</span>
         </h1>
-        <BrowserRouter>
+        <HashRouter>
           <div className="list container">
             <ul className="list-group">
               <li className="list-group-item">
-                <Link to="/">Home</Link>
+                <Link to="/home">Home</Link>
               </li>
               <li className="list-group-item">
                 <Link to="/about">About</Link>
               </li>
             </ul>
-            <Route exact path="/" component={Home} />
+          </div>
+          <div className="container">
+            <Route path="/home" component={Home} />
             <Route path="/about" component={About} />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   };
