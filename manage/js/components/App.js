41ed35df18c9d413bf07f13d0d9c2549b2be8c83
shuntaro shoji <160341ss@gmail.com>
2019-12-31 21:28:20 +0900

    add react router dom ※正しく動作しない状態

--- manage/js/components/App.js
+++ manage/js/components/App.js
@@ -5,18 +5,47 @@ define(function (require, exports, module) {
 
   var ReactDOM = require("react-dom");
 
-  var Sample = require("./Sample");
+  var BrowserRouter = require("react-router-dom").BrowserRouter;
 
-  var Card = require("./Card");
+  var Route = require("react-router-dom").Route;
+
+  var Link = require("react-router-dom").Link;
 
   var App = function App() {
+    var Home = function Home() {
+      return React.createElement("div", null, React.createElement("h2", null, "Home"), "Home\u30DA\u30FC\u30B8\u3067\u3059");
+    };
+
+    var About = function About() {
+      return React.createElement("div", null, React.createElement("h2", null, "About"), "About\u30DA\u30FC\u30B8\u3067\u3059");
+    };
+
     return React.createElement("div", {
       className: "app container"
     }, React.createElement("h1", {
       className: "display-1"
     }, "Hello World!", React.createElement("span", {
       className: "badge badge-secondary"
-    }, "New")), React.createElement(Sample, null));
+    }, "New")), React.createElement(BrowserRouter, null, React.createElement("div", {
+      className: "list container"
+    }, React.createElement("ul", {
+      className: "list-group"
+    }, React.createElement("li", {
+      className: "list-group-item"
+    }, React.createElement(Link, {
+      to: "/"
+    }, "Home")), React.createElement("li", {
+      className: "list-group-item"
+    }, React.createElement(Link, {
+      to: "/about"
+    }, "About"))), React.createElement(Route, {
+      exact: true,
+      path: "/",
+      component: Home
+    }), React.createElement(Route, {
+      path: "/about",
+      component: About
+    }))));
   };
 
   ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
 define(function(require, exports, module) {
   const React = require("react");
   const ReactDOM = require("react-dom");
-  const Sample = require("./Sample");
-  const Card = require("./Card");
+  const BrowserRouter = require("react-router-dom").BrowserRouter;
+  const Route = require("react-router-dom").Route;
+  const Link = require("react-router-dom").Link;
 
   const App = () => {
+    const Home = () => (
+      <div>
+        <h2>Home</h2>
+        Homeページです
+      </div>
+    );
+    const About = () => (
+      <div>
+        <h2>About</h2>
+        Aboutページです
+      </div>
+    );
+
     return (
       <div className="app container">
         <h1 className="display-1">
           Hello World!
           <span className="badge badge-secondary">New</span>
         </h1>
-        <Sample />
+        <BrowserRouter>
+          <div className="list container">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   };
